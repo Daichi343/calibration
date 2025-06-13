f4d053e601507478e8bca370d41e95ca9a3e928f
Daichi343 <naka.d0114nnp@gmail.com>
2025-06-13 20:55:32 +0900

    CとD追加

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -2,19 +2,15 @@ import cv2
 import numpy as np
 import glob
 
-# チェスボードの交点数
 CHECKERBOARD = (12, 7)
 
-# 3Dポイントと2Dポイントを格納するリスト
 objpoints = []
 imgpoints = []
 
-# 3Dポイントの準備（Z=0の平面上）
 objp = np.zeros((CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
 objp[:, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 
-# 画像の読み込み
-images = glob.glob('calib_images_B/*.jpg')
+images = glob.glob('calib_images_D/*.jpg')
 
 for fname in images:
     img = cv2.imread(fname)
@@ -30,15 +26,12 @@ for fname in images:
         )
         imgpoints.append(corners2)
 
-# カメラキャリブレーション
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
     objpoints, imgpoints, gray.shape[::-1], None, None)
 
-# 結果表示
 print("Camera Matrix (Intrinsic parameters):\n", mtx)
 print("\nDistortion Coefficients:\n", dist)
 
-# 再投影誤差の計算
 total_error = 0
 for i in range(len(objpoints)):
     imgpoints2, _ = cv2.projectPoints(
